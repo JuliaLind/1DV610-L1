cbe4691108f1f64bff04080600b2f0f007dc4926
Julia Lind <lind.julia@hotmail.com>
2025-09-03 00:59:29 +0200

    Create hello-box component

---
+++ src/js/components/hello-box/hello-box.js
@@ -0,0 +1,100 @@
+/**
+ * The hello-box component module.
+ *
+ * @author Julia Lind <jl225vf@student.lnu.se>
+ * @version 1.1.1
+ */
+
+import { randomFromArray } from "./lib/functions.js"
+
+const template = document.createElement('template')
+
+template.innerHTML = `
+    <style>
+        :host,
+        * {
+        box-sizing: border-box;
+        }
+    </style>
+    <div>
+        <header>
+            <button id="close-btn">✕</button>
+        </header>
+        <div>
+            <p><span id="greeting">Hello</span>, <span id="name"></span>!</p>
+        </div>
+    </div>
+`
+
+customElements.define('hello-box',
+    /**
+     * Represents a hello-box element.
+     */
+    class extends HTMLElement {
+        #greetingOptions = ['Hello', 'Hi', 'Hey', 'Greetings', 'Hey there']
+        #closeBtn
+        #greeting
+        #name
+
+        /**
+         * Creates an instance of current class.
+         */
+        constructor() {
+            super()
+
+            this.attachShadow({ mode: 'open' })
+                .append(template.content.cloneNode(true))
+            this.#closeBtn = this.shadowRoot.querySelector('#close-btn')
+            this.#greeting = this.shadowRoot.querySelector('#greeting')
+            this.#name = this.shadowRoot.querySelector('#name')
+        }
+
+        /**
+         * Called when the element is added to the document.
+         */
+        connectedCallback() {
+            this.#closeBtn.addEventListener('click', this.#onClose)
+        }
+
+        /**
+         * Handles the close button click.
+         *
+         * @param {MouseEvent} event - the click event fired when the close button is clicked.
+         */
+        #onClose = (event) => {
+            this.dispatchEvent(new CustomEvent('close', {
+                bubbles: true
+            }))
+        }
+
+        /**
+         * Called when the element is removed from the document.
+         */
+        disconnectedCallback() {
+            this.#closeBtn.removeEventListener('click', this.#onClose)
+        }
+
+        /**
+         * Attributes to monitor for changes.
+         *
+         * @returns {string[]} A string array of attributes to monitor.
+         */
+        static get observedAttributes() {
+            return ['name']
+        }
+
+        /**
+         * Called by the browser engine when an attribute changes.
+         *
+         * @param {string} name of the attribute.
+         * @param {any} oldValue the old attribute value.
+         * @param {any} newValue the new attribute value.
+         */
+        attributeChangedCallback (name, oldValue, newValue) {
+            if (name === 'name') {
+                this.#name.textContent = newValue
+                this.#greeting.textContent = randomFromArray(this.#greetingOptions)
+            }
+        }
+
+    })
---
+++ src/js/components/hello-box/lib/functions.js
@@ -0,0 +1,10 @@
+/**
+ * Returns a random element from the given array.
+ *
+ * @param {Array} arr - The array to select a random element from.
+ * @returns {*} A random element from the array.
+ */
+export function randomFromArray(arr) {
+    const index = Math.floor(Math.random() * arr.length)
+    return arr[index]
+}
